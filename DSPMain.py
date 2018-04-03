cccb982524b8aff9fa4484fa1e7182403670c07d
Maxwell Crawford <masamichi7@outlook.com>
2018-04-03 19:26:46 -0400

    FFT vers1

--- DSPMain.py
+++ DSPMain.py
@@ -1,22 +1,32 @@
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt # for graphing
+import cmath                    # for complex number funcs
+from math import sin, pi
 import DSPToolkit as DT
+import numpy as np # TEST
 
 def main():
     # Create wave objects
     wave1 = DT.DSPWave() # default settings
     wave2 = DT.DSPWave(1.2, 2, 0.1, 0.1)
+    coswave = DT.DSPWave(1, 1, pi / 2.0, 0) # only phase shift!
+    # coswave = DT.DSPWave() #TEST
 
     # Get wave x/y axes:
     w1x = wave1.generateTimeSet()
-    w1y = wave1.generateSampleSet()
+    w1y = wave1.generateSineSampleSet()
     w2x = wave2.generateTimeSet()
-    w2y = wave2.generateSampleSet()
+    w2y = wave2.generateSineSampleSet()
+    cosx = coswave.generateTimeSet()
+    cosy = coswave.generateSineSampleSet() # was sine...
+    # cosy = coswave.generateCosineSampleSet()
 
     # Get complex waveforms:
     cwave1 = DT.waveAddition(w1y, w2y)
     cwave2 = DT.waveMultiplication(w1y, w2y)
 
     # Plot wave 1:
+    print("\n\t * Sine Wave 1")
+    plt.title('Sine Wave 1')
     plt.plot(w1x, w1y, 'b-') # blue line
     xmax = max(w1x) * 1.5
     xmin = min(w1x)
@@ -30,11 +40,13 @@ def main():
         ymin *= 1.5
     else:
         ymin *= -1.5
-    axis_list = [xmin, xmax, ymin, ymax] #xmin
+    axis_list = [xmin, xmax, ymin, ymax]
     plt.axis(axis_list)
     plt.show()
 
     # Plot wave 2:
+    print("\n\t * Sine Wave 2")
+    plt.title('Sine Wave 2')
     plt.plot(w2x, w2y, 'r-') # red line
     xmax = max(w2x) * 1.5
     xmin = min(w2x)
@@ -48,15 +60,39 @@ def main():
         ymin *= 1.5
     else:
         ymin *= -1.5
-    axis_list = [xmin, xmax, ymin, ymax] #xmin
+    axis_list = [xmin, xmax, ymin, ymax]
+    plt.axis(axis_list)
+    plt.show()
+
+    # Plot cosine wave:
+    print("\n\t * Cosine Wave")
+    plt.title('Cosine Wave')
+    plt.plot(cosx, cosy, 'g-') # green line
+    xmax = max(cosx) * 1.5
+    xmin = min(cosx)
+    if xmin < 0:
+        xmin *= 1.5
+    else:
+        xmin *= -1.5
+    ymax = max(cosy) * 1.5
+    ymin = min(cosy)
+    if ymin < 0:
+        ymin *= 1.5
+    else:
+        ymin *= -1.5
+    axis_list = [xmin, xmax, ymin, ymax]
     plt.axis(axis_list)
     plt.show()
 
     # Plot wave 1 and wave 2:
+    print("\n\t * Sine Waves 1 and 2")
+    plt.title('Sine Waves 1 and 2')
     plt.plot(w1x, w1y, 'b-', w2x, w2y, 'r-')
     plt.show()
 
     # Plot complex wave 1 (add):
+    print("\n\t * Complex Wave 1 - Addition")
+    plt.title('Complex Wave 1 - Addition')
     if len(w1x) < len(w2x):
         plt.plot(w1x, cwave1, 'bs')
         plt.show()
@@ -65,12 +101,164 @@ def main():
         plt.show()
 
     # Plot complex wave 2 (mult):
+    print("\n\t * Complex Wave 2 - Multiplication")
+    plt.title('Complex Wave 2 - Multiplication')
     if len(w1x) < len(w2x):
         plt.plot(w1x, cwave2, 'bs')
         plt.show()
     else:
         plt.plot(w2x, cwave2, 'bs')
         plt.show()
+
+    #--
+    # FFT TEST
+    fft_data = [26160.0, 19011.0, 18757.0, 18405.0, 17888.0, 14720.0, 14285.0,17018.0, 18014.0, 17119.0, 16400.0, 17497.0, 17846.0, 15700.0, 17636.0, 17181.0] #NEW
+    print("\n\t * FFT TEST")
+    fft_cwave1 = DT.fft_cooley(cwave1)
+    fft_cwave1test = np.fft.fft(cwave1)
+    fft_wave = DT.fft_cooley(w1y)
+    fft_wavetest = np.fft.fft(w1y)
+
+    # fft_real = [x.real for x in fft_cwave1]
+    fft_real = [x.real for x in fft_wave]
+    
+    # plt.title('Complex Wave 1 FFT')
+    # if len(w1x) < len(w2x):
+    #     plt.plot(w1x, fft_real, 'go')
+    #     plt.show()
+    # else:
+    #     plt.plot(w2x, fft_real, 'go')
+    #     plt.show()
+
+    plt.title('Wave 1 FFT')
+    plt.plot(w1x, fft_real, 'go')
+    plt.show()
+    
+    # fft_real = [x.real for x in fft_cwave1test]
+    # plt.title('Complex Wave 1 FFT numpy vers')
+    # if len(w1x) < len(w2x):
+    #     plt.plot(w1x, fft_real, 'go')
+    #     plt.show()
+    # else:
+    #     plt.plot(w2x, fft_real, 'go')
+    #     plt.show()
+
+    fft_real = [x.real for x in fft_wavetest]
+    plt.title('Wave 1 FFT numpy vers')
+    plt.plot(w1x, fft_real, 'go')
+    plt.show()
+
+    #NEW
+    fft_complex = [complex(x) for x in fft_data]
+    print("FFT of Verification Data: ")
+    fft_verification = DT.fft_cooley(fft_complex)
+    with open("FFT_verification_TEST.txt", "w+") as f:
+        f.write("FFT of Verification Data:\n")
+        for d in fft_verification:
+            line = str(d)
+            line = line[1:-1] # get rid of ()
+            line = line.replace('j', 'i') # convert back to std. form
+            f.write(line + "\n")
+    
+    fft_complex = [complex(x) for x in w1y] #SINE WAVE1
+    print("FFT of Sine Wave: ")
+    fft_verification = DT.fft_cooley(fft_complex)
+    with open("FFT_sine_TEST.txt", "w+") as f:
+        f.write("Sine Data Points:\n")
+        for d in w1y:
+            f.write(str(d) + "\n")
+        f.write("FFT of Sine Wave Data:\n")
+        for d in fft_verification:
+            line = str(d)
+            line = line[1:-1] # get rid of ()
+            line = line.replace('j', 'i') # convert back to std. form
+            f.write(line + "\n")
+
+    #--
+    # Problem 1a TEST
+    S = [5, 10, 100]
+    N = 512
+    t1 = 1/N
+    tlist = []
+    for t in range(N): # generate samples
+        tlist.append(t1*t)
+    
+    fst1 = [] # list of values -> 512 samples, each sample being k=1 thru s
+    fst2 = []
+    fst3 = []
+
+    gst1 = []
+    gst2 = []
+    gst3 = []
+
+    # Fs(t)
+    # OUTER LOOP = t
+    for t in range(N):
+        fst_current = 0.0 # hold sum of k->s
+        # INNER LOOP = k->s
+        for k in range(1, S[0]):
+            fst_current += (sin(DT.TWO_PI*(2*k-1)*t)) / (2*k-1)
+        fst1.append(fst_current)
+    for t in range(N):
+        fst_current = 0.0 # hold sum of k->s
+        # INNER LOOP = k->s
+        for k in range(1, S[1]):
+            fst_current += (sin(DT.TWO_PI*(2*k-1)*t)) / (2*k-1)
+        fst2.append(fst_current)
+    for t in range(N):
+        fst_current = 0.0 # hold sum of k->s
+        # INNER LOOP = k->s
+        for k in range(1, S[2]):
+            # temp = sin(DT.TWO_PI*(2*k-1)) / (2*k-1)
+            # print(temp)
+            fst_current += (sin(DT.TWO_PI*(2*k-1)*t)) / (2*k-1)
+        fst3.append(fst_current)
+    #TEST
+    print("Fs(t) 1:")
+    with open("fst.txt", "w+") as f:
+        f.write("Fs(t):\n")
+        for i in range(len(fst1)):
+            f.write(str(fst1[i]))
+            f.write("\n")
+    
+    #Gs(t)
+    # OUTER LOOP = t
+    for t in range(N):
+        gst_current = 0.0 # hold sum of k->s
+        # INNER LOOP = k->s
+        for k in range(1, S[0]):
+            gst_current += (sin(DT.TWO_PI*(2*k)*t)) / (2*k)
+        gst1.append(gst_current)
+    for t in range(N):
+        gst_current = 0.0 # hold sum of k->s
+        # INNER LOOP = k->s
+        for k in range(1, S[1]):
+            gst_current += (sin(DT.TWO_PI*(2*k)*t)) / (2*k)
+        gst2.append(gst_current)
+    for t in range(N):
+        gst_current = 0.0 # hold sum of k->s
+        # INNER LOOP = k->s
+        for k in range(1, S[2]):
+            # temp = sin(DT.TWO_PI*(2*k-1)) / (2*k)
+            # print(temp)
+            gst_current += (sin(DT.TWO_PI*(2*k-1)*t)) / (2*k)
+        gst3.append(gst_current)
+
+    plt.title('Problem 1a Complex Wave')
+    plt.plot(tlist, fst3, 'ro') # red dots
+    xmin = 0
+    xmax = N
+    
+    ymax = max(fst3) * 1.5
+    ymin = min(fst3)
+    if ymin < 0:
+        ymin *= 1.5
+    else:
+        ymin *= -1.5
+    axis_list = [xmin, xmax, ymin, ymax] #xmin
+    plt.axis(axis_list)
+    plt.show()
+    #--
 
 if __name__ == "__main__":
     main()
--- DSPToolkit.py
+++ DSPToolkit.py
@@ -1,11 +1,24 @@
 import math
+import cmath # for complex nums
+from copy import deepcopy
+import numpy as np
+
+
+# GLOBALS
+TWO_PI = math.pi * 2.0 # only compute once
+ONE_PI = math.pi
 
 class DSPWave(object):
     """A set of parameters and functions for
     a DSP Wave signal.
-    
+
+    NOTE: The default signal is a sine wave.
+
     Arguments:
-        object {[type]} -- [description]
+        a {int} -- amplitude (default: {1})
+        f {int} -- frequency in Hz (default: {1})
+        c {int} -- phase shift (default: {0})
+        d {int} -- vertical translation or bias (default: {0})
     """
     def __init__(self, a=1, f=1, c=0, d=0):
         """Initialize the wave with key components.
@@ -16,21 +29,51 @@ class DSPWave(object):
             c {int} -- phase shift (default: {0})
             d {int} -- vertical translation or bias (default: {0})
         """
+        # Set of wave properties
         self.amplitude = a
         self.frequency = f
         self.phase = c          # c; left/right
         self.translation = d    # d; down/up
-        self.two_pi = math.pi * 2.0 # only compute once!
-
+        
+        # Set of useful calcs
+        # self.two_pi = math.pi * 2.0 # only compute once!
+        self.two_pi = TWO_PI
         self.nyquist_rate = self.frequency * 2.0
 
-    def generateWaveResult(self, t=0):
-        """Generate f(t) result at the given t (time interval).
+        # TEST: new properties...
+        # self.B = self.two_pi / f
+        # self.C = -1.0 * c / self.B
+
+    def generateSineWaveResult(self, t=0):
+        """Generate f(t) result at the given t (time interval),
+        using sine wave form:
+        f(t) = a*sin(2pi*f(t-c)) + d
 
         Keyword Arguments:
             t {int} -- time interval (default: {0})
         """
         return self.amplitude * math.sin(self.two_pi * self.frequency * (t -    self.phase)) + self.translation
+
+    # def generateSineWaveResult2(self, t=0):
+    #     """Generate f(t) result at the given t (time interval),
+    #     using sine wave form:
+    #     f(t) = y = A sin(Bx + C) + D,
+    #     where Amplitude: A, Frequency: 2pi/B, Phase: -C/B, Vert. Shift: D
+
+    #     Keyword Arguments:
+    #         t {int} -- time interval (default: {0})
+    #     """
+    #     return self.amplitude * math.sin(self.B * t + self.C) + self.translation
+    
+    def generateCosineWaveResult(self, t=0):
+        """Generate f(t) result at the given t (time interval),
+        using cosine wave form:
+        f(t) = a*cos(2pi*f(t-c)) + d
+
+        Keyword Arguments:
+            t {int} -- time interval (default: {0})
+        """
+        return self.amplitude * math.cos(self.two_pi * self.frequency * (t -    self.phase)) + self.translation
 
     def generateTimeSet(self, samples=64, time_interval=0.015625):
         """Generate a list of evenly-spaced time intervals
@@ -46,12 +89,14 @@ class DSPWave(object):
             samples = 64 # could later change to self.nyquist_rate!
 
         time_set = []
-        for n in range(1, samples + 1): # note the (1,n)
+        # for n in range(1, samples + 1): # note the (1,n)
+        #     time_set.append(n * time_interval)
+        for n in range(samples): # WAS (1, samples+1)
             time_set.append(n * time_interval)
         return time_set
 
-    def generateSampleSet(self, samples=64, time_interval=0.015625):
-        """Generate a list of Wave results, by computing
+    def generateSineSampleSet(self, samples=64, time_interval=0.015625):
+        """Generate a list of Sine Wave results, by computing
         wave results per evenly-spaced interval for n samples.
         NOTE: These results correlate to the y-axis on a graph/plot.
 
@@ -64,8 +109,27 @@ class DSPWave(object):
             samples = 64 # could later change to self.nyquist_rate!
 
         sample_set = []
-        for n in range(1, samples + 1): # note the (1,n)
-            sample_set.append(self.generateWaveResult(n * time_interval))
+        for n in range(samples): # WAS (1,samples+1)
+            sample_set.append(self.generateSineWaveResult(n * time_interval))
+            # sample_set.append(self.generateSineWaveResult2(n * time_interval))
+        return sample_set
+    
+    def generateCosineSampleSet(self, samples=64, time_interval=0.015625):
+        """Generate a list of Cosine Wave results, by computing
+        wave results per evenly-spaced interval for n samples.
+        NOTE: These results correlate to the y-axis on a graph/plot.
+
+        Keyword Arguments:
+            samples {int} -- n number of samples to capture (default: {64})
+            time_interval {float} -- the interval of time change per sample         (default: {0.015625})
+        """
+        # Ensure samples are high enough:
+        if samples < 64:
+            samples = 64 # could later change to self.nyquist_rate!
+
+        sample_set = []
+        for n in range(samples): # WAS (1,samples+1)
+            sample_set.append(self.generateCosineWaveResult(n * time_interval))
         return sample_set
 
 # AUX FUNCS
@@ -110,3 +174,147 @@ def waveMultiplication(waveset1, waveset2):
         for s in range(waveset2len):
             complex_set.append(waveset1[s] * waveset2[s])
     return complex_set
+
+# Fourier stuff
+# LINK: https://jakevdp.github.io/blog/2013/08/28/understanding-the-fft/
+def DFT_slow(x):
+    """Compute the discrete Fourier Transform of the 1D array x"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    n = np.arange(N)
+    k = n.reshape((N, 1))
+    M = np.exp(-2j * np.pi * k * n / N)
+    return np.dot(M, x)
+
+def FFT(x):
+    """A recursive implementation of the 1D Cooley-Tukey FFT"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+    
+    if N % 2 > 0:
+        raise ValueError("size of x must be a power of 2")
+    elif N <= 32:  # this cutoff should be optimized
+        return DFT_slow(x)
+    else:
+        X_even = FFT(x[::2])
+        X_odd = FFT(x[1::2])
+        factor = np.exp(-2j * np.pi * np.arange(N) / N)
+        return np.concatenate([X_even + factor[:N / 2] * X_odd,
+                               X_even + factor[N / 2:] * X_odd])
+
+def FFT_vectorized(x):
+    """A vectorized, non-recursive version of the Cooley-Tukey FFT"""
+    x = np.asarray(x, dtype=float)
+    N = x.shape[0]
+
+    if np.log2(N) % 1 > 0:
+        raise ValueError("size of x must be a power of 2")
+
+    # N_min here is equivalent to the stopping condition above,
+    # and should be a power of 2
+    N_min = min(N, 32)
+    
+    # Perform an O[N^2] DFT on all length-N_min sub-problems at once
+    n = np.arange(N_min)
+    k = n[:, None]
+    M = np.exp(-2j * np.pi * n * k / N_min)
+    X = np.dot(M, x.reshape((N_min, -1)))
+
+    # build-up each level of the recursive calculation all at once
+    while X.shape[0] < N:
+        X_even = X[:, :X.shape[1] / 2]
+        X_odd = X[:, X.shape[1] / 2:]
+        factor = np.exp(-1j * np.pi * np.arange(X.shape[0])
+                        / X.shape[0])[:, None]
+        X = np.vstack([X_even + factor * X_odd,
+                       X_even - factor * X_odd])
+
+    return X.ravel() # return flattened, contiguous 1D array of elems
+
+# LINK: https://rosettacode.org/wiki/Fast_Fourier_transform#Python
+def fft_rosetta(x):
+    N = len(x)
+    if N <= 1: return x
+    even = fft_rosetta(x[0::2])
+    odd = fft_rosetta(x[1::2])
+    T = [cmath.exp(-2j*cmath.pi*k/N)*odd[k] for k in range(N//2)]
+    return [even[k] + T[k] for k in range(N//2)] + \
+           [even[k] - T[k] for k in range(N//2)]
+
+def fft_cooley(data, direction=1):
+    """Get the Fast Fourier Transform of list data,
+    using a recursive form of the Cooley-Tukey Algorithm.
+        
+    Arguments:
+        data {list} -- A list of y-data, generally from a signal/wave
+    
+    Keyword Arguments:
+        direction {int} -- 1 for regular, -1 for inverse (default: {1})
+    
+    Returns:
+        list -- A list of new FT data
+    """
+    N = len(data)
+    if N <= 1:
+        return data
+    
+    even = fft_cooley(data[0::2])
+    odd = fft_cooley(data[1::2])
+    twiddles = [cmath.exp(direction * -2j*cmath.pi*k/N)*odd[k] for k in range(N//2)]
+
+    return [even[k] + twiddles[k] for k in range(N//2)] + \
+           [even[k] - twiddles[k] for k in range(N//2)]
+
+def two_dim_fft(data, xlen, ylen, direction=1):
+    """Get a 2D FFT of a set of data.
+    
+    Arguments:
+        data {list} -- Given 2D data for iteration
+        xlen {int} -- X dimension
+        ylen {int} -- Y dimension
+    
+    Keyword Arguments:
+        direction {int} -- 1 for regular, -1 for inverse (default: {1})
+    
+    Returns:
+        list -- A 2D list of FT values
+    """
+
+    result = []
+    inner_result = []
+
+    for x in range(xlen):
+        inner_result.clear()
+        for y in range(ylen):
+            inner_result.append(fft_cooley(data[y], direction))
+        result.append(deepcopy(inner_result)) # needed to retain cleared objs
+    return result
+    
+# def FFTCooleyTukey(data, N=64, direction=1):
+#     """Performs a recursive Fast Fourier Transform
+#     using the Cooley-Tukey method, on a list of doubles.
+#     Number of samples must be a power of two, and the
+#     direction can be positive (regular) or negative (inverse).
+    
+#     Arguments:
+#         data {list} -- a 1D list of doubles/floats/integers
+    
+#     Keyword Arguments:
+#         N {int} -- Number of samples taken (default: {64})
+#         direction {int} -- The direction of the transform; 
+#         regular or inverse (default: {1})
+#     """
+#     theta = (-1*TWO_PI*direction)/N
+#     r = N/2
+#     j = complex(0, 1) # eq. to cmath.sqrt(-1)
+
+#     # Loop 1: calculate Discrete Fourier Transform:
+#     # EMULATE DO WHILE LOOP
+#     i = 1
+#     while i < (N-1):
+#         w = math.cos(i*theta) + j * math.sin(i*theta) # w becomes complex!
+#         k = 0
+#         while k < (N-1):
+#             u = 1
+#             for m in range(r-1):
+#                 t = FFTCooleyTukey(data, k+m) - FFTCooleyTukey(data, k+m+r)
